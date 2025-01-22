ee68ff4db40b5dd55e1ce8c85eed4d18568911b3
fabgal-p1 <>
2025-01-22 18:01:06 +0100

    - add helpers_pandas.py

--- setup.py
+++ setup.py
@@ -1,10 +1,12 @@
+# pip install setuptools
+
 from setuptools import setup, find_packages
 
-requirements = ["toml", "PyYAML"]
+requirements = ["toml", "PyYAML", "pandas", "sqlalchemy", "numpy"]
 
 setup(
     name="helpers",
-    version="1.2",
+    version="1.4",
     install_requires=requirements,
     packages=find_packages(),
     author="fab",
